03ea71ad06ddde44be77fb144535e5a83b0cee7d
nourbenecheikh <148497774+nourbenecheikh@users.noreply.github.com>
2025-07-03 10:46:49 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8800, () => {
+app.listen(8800, '0.0.0.0',() => {
   connect();
   console.log("Backend server is running!");
 });
