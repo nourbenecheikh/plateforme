5fafaad40c2b662facb2ce8f9dc8f9b322dbc8eb
nourbenecheikh <148497774+nourbenecheikh@users.noreply.github.com>
2025-07-05 10:45:46 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connexion MongoDB + Démarrage du serveur
-const startServer = async () => {
+const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Connected to MongoDB!");
