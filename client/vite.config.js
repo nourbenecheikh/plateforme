14bee3b56a19e3cef664949a29fe16d0b43042bf
nourbenecheikh <148497774+nourbenecheikh@users.noreply.github.com>
2025-08-04 08:44:12 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
+import React, { useEffect, useState } from 'react';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import React, { useEffect, useState } from 'react';
 
 
 // https://vitejs.dev/config/
