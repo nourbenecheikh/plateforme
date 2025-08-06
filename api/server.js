8613eca757e10ec412a0aab4b36a5438834e65d2
nourbenecheikh <148497774+nourbenecheikh@users.noreply.github.com>
2025-08-06 09:18:56 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 
 
 
