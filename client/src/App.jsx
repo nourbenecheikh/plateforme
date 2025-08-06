e905be681084bd59d2474e1fb2dfd1a4b64f4e1b
nourbenecheikh <148497774+nourbenecheikh@users.noreply.github.com>
2025-08-06 09:13:13 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import "./app.scss";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import React from "react";
