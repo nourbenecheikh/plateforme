00e54d006481759e1fda9d1db73cdedb2f66e4c9
nourbenecheikh <nourbenecheikh3@gmail.com>
2025-08-06 11:12:53 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./app.scss";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
