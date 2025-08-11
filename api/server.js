710abbd61bb66affd34a414f891d6968e9950521
nourbenecheikh <nourbenecheikh3@gmail.com>
2025-08-11 12:30:48 +0100

    server.js

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ mongoose.set("strictQuery", true);
 
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
